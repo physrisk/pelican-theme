a831fad73882cc27424dd3c355bec6c342297006
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2020-11-02 13:03:55 +0200

    replace jQuery with vanilla JS DOM manipulation

--- static/js/application.js
+++ static/js/application.js
@@ -1,9 +1,15 @@
-$(function () {
-    $.ajax({url:"/theme/img/main-bg.svg"}).done(function() {
-        $("body").css("background-image","url(\"/theme/img/main-bg.svg\")")
-                 .css("background-repeat","repeat-y")
-                 .css("background-position","center top")
-                 .css("background-size","100% auto");
-    });
-    $("#js-load-sidebar").load("/sidebar-widgets.html");
-});
+function onLoad() {
+    fetch("/theme/img/main-bg.svg")
+        .then((response)=> {
+                bodyObj=document.querySelector("body");
+                bodyObj.style.backgroundImage="url(\"/theme/img/main-bg.svg\")";
+                bodyObj.style.backgroundRepeat="repeat-y";
+                bodyObj.style.backgroundPosition="center top";
+                bodyObj.style.backgroundSize="100% auto";
+            });
+    fetch("/sidebar-widgets.html")
+        .then((response) => response.text())
+        .then((str) => document.querySelector("#js-load-sidebar").innerHTML=str);
+}
+
+document.addEventListener('DOMContentLoaded', () => onLoad(), false);
