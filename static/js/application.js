0a5ea65caaaa831f2995935c3f57b81894d31f74
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2023-05-05 13:05:20 +0300

    update background loading

--- static/js/application.js
+++ static/js/application.js
@@ -1,22 +1,39 @@
 function doSearch() {
     let fieldObj;
-    fieldObj=document.querySelector("#searchInput");
-    fieldObj.value=fieldObj.value+" site:rf.mokslasplius.lt";
+    fieldObj = document.querySelector("#searchInput");
+    fieldObj.value = fieldObj.value + " site:rf.mokslasplius.lt";
     return true;
 }
 
-function onLoad() {
-    fetch("/theme/img/main-bg.svg")
-        .then(() => {
-                let bodyObj;
-                bodyObj=document.querySelector("body");
-                bodyObj.style.backgroundImage="url(\"/theme/img/main-bg.svg\")";
-                bodyObj.style.backgroundRepeat="repeat-y";
-                bodyObj.style.backgroundPosition="center top";
-                bodyObj.style.backgroundSize="100% auto";
-            });
+function getBackground() {
+    if (
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+        fetch("/theme/img/main-bg-dark.svg").then(() => {
+            let bodyObj;
+            bodyObj = document.querySelector("body");
+            bodyObj.style.backgroundImage =
+                'url("/theme/img/main-bg-dark.svg")';
+            bodyObj.style.backgroundRepeat = "repeat-y";
+            bodyObj.style.backgroundPosition = "center top";
+            bodyObj.style.backgroundSize = "100% auto";
+        });
+    } else {
+        fetch("/theme/img/main-bg.svg").then(() => {
+            let bodyObj;
+            bodyObj = document.querySelector("body");
+            bodyObj.style.backgroundImage = 'url("/theme/img/main-bg.svg")';
+            bodyObj.style.backgroundRepeat = "repeat-y";
+            bodyObj.style.backgroundPosition = "center top";
+            bodyObj.style.backgroundSize = "100% auto";
+        });
+    }
 }
 
-document.addEventListener("DOMContentLoaded", () => onLoad(), false);
+document.addEventListener("DOMContentLoaded", () => getBackground(), false);
+window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", () => getBackground());
 
-window.MathJax={tex:{tags:'ams'}};
+window.MathJax = { tex: { tags: "ams" } };
