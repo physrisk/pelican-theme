210dc27468948466fe2fe50d96edbabdd42999fc
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2023-11-28 14:10:21 +0200

    js: DRY getBackground function

--- static/js/application.js
+++ static/js/application.js
@@ -6,29 +6,20 @@ function doSearch() {
 }
 
 function getBackground() {
+    let backgroundImage = "/theme/img/main-bg.svg";
     if (
         window.matchMedia &&
         window.matchMedia("(prefers-color-scheme: dark)").matches
     ) {
-        fetch("/theme/img/main-bg-dark.svg").then(() => {
-            let bodyObj;
-            bodyObj = document.querySelector("body");
-            bodyObj.style.backgroundImage =
-                'url("/theme/img/main-bg-dark.svg")';
-            bodyObj.style.backgroundRepeat = "repeat-y";
-            bodyObj.style.backgroundPosition = "center top";
-            bodyObj.style.backgroundSize = "100% auto";
-        });
-    } else {
-        fetch("/theme/img/main-bg.svg").then(() => {
-            let bodyObj;
-            bodyObj = document.querySelector("body");
-            bodyObj.style.backgroundImage = 'url("/theme/img/main-bg.svg")';
-            bodyObj.style.backgroundRepeat = "repeat-y";
-            bodyObj.style.backgroundPosition = "center top";
-            bodyObj.style.backgroundSize = "100% auto";
-        });
+        backgroundImage = "/theme/img/main-bg-dark.svg";
     }
+    fetch(backgroundImage).then(() => {
+        let bodyObj = document.querySelector("body");
+        bodyObj.style.backgroundImage = `url("${backgroundImage}")`;
+        bodyObj.style.backgroundRepeat = "repeat-y";
+        bodyObj.style.backgroundPosition = "center top";
+        bodyObj.style.backgroundSize = "100% auto";
+    });
 }
 
 document.addEventListener("DOMContentLoaded", () => getBackground(), false);
